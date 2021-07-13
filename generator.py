55dc231f05d65b2fca72d5eda7764149ceb4c744
ThePhilosopher37 <75205458+ThePhilosopher37@users.noreply.github.com>
2021-07-14 02:17:49 +0530

    adding open mine bias

--- generator.py
+++ generator.py
@@ -5,7 +5,7 @@ MAX_SIZE = 25
 
 class mine:
 
-    def __init__(self, size, difficulty):
+    def __init__(self, size, difficulty, seed):
         if (size < 5 or size > MAX_SIZE):
             self.exist = False
         elif (difficulty < 0 or difficulty > size**2):
@@ -15,15 +15,29 @@ class mine:
             self.diff = difficulty
             self.size = size
             self.state = np.zeros((size, size), dtype = 'i')
+            seed_x, seed_y = seed
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if ((seed_x+i < 0) or (seed_y+j >= self.size) or (seed_y+j < 0) or (seed_x+i >= self.size)):
+                        continue
+                    self.state[seed_x + i][seed_y + j] = -2
             while(difficulty!=0):
                 place_x = random.randint(0, size - 1)
                 place_y = random.randint(0, size - 1)
-                while (self.state[place_x][place_y] == -1):
+                while (self.state[place_x][place_y] == -1 or self.state[place_x][place_y] == -2):
                     place_x = random.randint(0, size - 1)
                     place_y = random.randint(0, size - 1)
                 self.state[place_x][place_y] = -1
                 self.adder (place_x, place_y)
                 difficulty -= 1
+            # final -2 compensation
+            for i in range(-1, 2):
+                for j in range(-1, 2):
+                    if ((seed_x+i < 0) or (seed_y+j >= self.size) or (seed_y+j < 0) or (seed_x+i >= self.size)):
+                        continue
+                    self.state[seed_x + i][seed_y + j] += 2
+
+            self.state[seed_x][seed_y] = -2
 
     def adder(self, a, b):
         for i in range(-1, 2):
@@ -35,3 +49,5 @@ class mine:
                 elif (self.state[a + i][b + j] == -1):
                     continue
                 self.state[a + i][b + j] += 1
+
+# method of shifting 0 clusters can be tried
